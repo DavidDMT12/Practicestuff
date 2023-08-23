0bf459603de078a201e76d61cd97789f5d7c8b93
deliveredmetal <118897748+DavidDMT12@users.noreply.github.com>
2023-08-23 11:37:17 +0200

    Update generatekeys.js
    Generates keys and address

--- week1 AU/generatekeys.js
+++ week1 AU/generatekeys.js
@@ -1,5 +1,6 @@
 const secp = require("ethereum-cryptography/secp256k1");
 const { keccak256 } = require("ethereum-cryptography/keccak");
+const { toHex } = require("ethereum-cryptography/utils")
 
 function getAddress(publicKey) {
     const slicedKey = publicKey.slice(1); // Remove the first byte (prefix)
@@ -7,12 +8,12 @@ function getAddress(publicKey) {
     const hashedKeyBytes = new Uint8Array(hashedKey); // Convert hash to Uint8Array
     const address = hashedKeyBytes.slice(hashedKeyBytes.length - 20); // Take the last 20 bytes
 
-    return address;
+    return toHex(address); // Convert the address to hexadecimal
 }
 
 function generateKeyPair() {
-    const privateKey = secp.utils.randomPrivateKey(); // Generate a random private key
-    const publicKey = secp.getPublicKey(privateKey, false); // Get the corresponding public key
+    const privateKey = secp.secp256k1.utils.randomPrivateKey(); // Generate a random private key
+    const publicKey = secp.secp256k1.getPublicKey(privateKey, false); // Get the corresponding public key
 
     const address = getAddress(publicKey);
 
@@ -20,8 +21,8 @@ function generateKeyPair() {
     const last20Bytes = publicKey.slice(-20);
 
     return {
-        privateKey,
-        publicKey,
+        privateKey: toHex(privateKey), // Convert private key to hexadecimal
+        publicKey: toHex(publicKey),   // Convert public key to hexadecimal
         address
     };
 }

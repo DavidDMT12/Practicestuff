cf27229342f9d2f142c043802ac4c35b0dd695da
deliveredmetal <118897748+DavidDMT12@users.noreply.github.com>
2023-08-25 22:14:43 +0200

    Update testsigner.js
    change of library

--- week1 AU/testsigner.js
+++ week1 AU/testsigner.js
@@ -1,21 +1,22 @@
-const secp = require("ethereum-cryptography/secp256k1");
-//import * as secp from '@noble/secp256k1';
-const { keccak256 } = require("ethereum-cryptography/keccak");
-const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+const EthCrypto = require('eth-crypto');
 
-const privateKey = "744e482805445f21f27bf1edefcb4f82ef7cfd2b26dd149c19eb3465c6738646";
+const identity = EthCrypto.createIdentity();
 const sendAmount = 2;
 const recipient = "0x9876543210fedcba9876543210fedcba98765432";
 
 
 const hashData = recipient + sendAmount;
 
-const messageHash = keccak256(utf8ToBytes(hashData));
-const signedMessage = secp.secp256k1.sign(messageHash, privateKey, { extraEntropy: true });
+const messageHash = EthCrypto.hash.keccak256(hashData);
+const signature = EthCrypto.sign(identity.privateKey, messageHash);
+const publicKey = EthCrypto.recoverPublicKey(signature, messageHash);
+const address = EthCrypto.publicKey.toAddress(publicKey);
+const check = address === identity.address;
 
-const publicKey = toHex(signedMessage.recoverPublicKey(messageHash).toRawBytes());
-const valid = secp.secp256k1.verify(signedMessage, messageHash, publicKey) === true;
 
-console.log("signature:", signedMessage)
+
+
+console.log("signature:", signature)
 console.log("publickey:", publicKey)
-console.log("isValid:", valid)
+console.log(check)
+console.log(identity)

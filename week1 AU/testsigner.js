c1d8833d6f6e3bb452a5f52302263ccfb9f76234
deliveredmetal <118897748+DavidDMT12@users.noreply.github.com>
2023-08-25 14:55:08 +0200

    Update testsigner.js
    Working signer

--- week1 AU/testsigner.js
+++ week1 AU/testsigner.js
@@ -1,16 +1,21 @@
 const secp = require("ethereum-cryptography/secp256k1");
+//import * as secp from '@noble/secp256k1';
 const { keccak256 } = require("ethereum-cryptography/keccak");
-const { toHex,utf8ToBytes } = require("ethereum-cryptography/utils");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 
-//const privateKey = utf8ToBytes("0fad76a6b07ef8dd6a0bc814b6339d895855881acaf3c1f901c6744368936bb3"); // Replace with your actual private key
+const privateKey = "744e482805445f21f27bf1edefcb4f82ef7cfd2b26dd149c19eb3465c6738646";
 const sendAmount = 2;
-const balance = 3;
-const recipient = "0x9876543210fedcba9876543210fedcba98765432"; // Replace with your actual recipient address
-const hashData = recipient + sendAmount + balance;
+const recipient = "0x9876543210fedcba9876543210fedcba98765432";
 
 
-const privateKey = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+const hashData = recipient + sendAmount;
+
 const messageHash = keccak256(utf8ToBytes(hashData));
-const signedMessage = secp.secp256k1.sign(messageHash, privateKey);
-const signresult = signedMessage;
-console.log("Signed Message:", signresult);
+const signedMessage = secp.secp256k1.sign(messageHash, privateKey, { extraEntropy: true });
+
+const publicKey = toHex(signedMessage.recoverPublicKey(messageHash).toRawBytes());
+const valid = secp.secp256k1.verify(signedMessage, messageHash, publicKey) === true;
+
+console.log("signature:", signedMessage)
+console.log("publickey:", publicKey)
+console.log("isValid:", valid)
